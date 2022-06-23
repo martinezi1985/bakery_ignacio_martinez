f5ac6d3fff86752bae4c410556f7b9edfcfccfe0
Ignacio Gabriel Martinez <martinezi1985@gmail.com>
2022-06-23 15:00:44 -0300

    Añado imagenes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let ingreso = parseInt(prompt("Ingresa el número de la promoción que buscas (del 1 al 4)"));
+/* let ingreso = parseInt(prompt("Ingresa el número de la promoción que buscas (del 1 al 4)"));
 let promocion1 = 1;
 let promocion2 = 2;
 let promocion3 = 3;
@@ -15,4 +15,96 @@ if (ingreso == 1) {
 }
 else {
     alert("Error, debes elegir del 1 al 4 para obtener dicha promoción");
+} */
+
+
+
+/* for (torta = 1; torta <= 5; torta++) {
+    
+        let torta = parseInt(prompt("Ingrese su torta preferida (Opciones 1 a 5)"));
+        let precioTorta = parseFloat(prompt("Ingrese el precio de la torta elegida"));
+
+        alert("Has elegido la torta: " + torta + " y su precio es: $" + precioTorta)
+        console.log(torta)
+        console.log(precioTorta)
+} */
+
+// Integrador
+
+function saludo() {
+  alert("Bienvenido a Bakery Sunshine!");
+  let nombre = prompt("Ingrese su nombre");
+
+  while (nombre === "") {
+    nombre = prompt("Ingrese su nombre");
+  }
+  return nombre;
 }
+
+function listadoProductos() {
+  let producto;
+  do {
+    producto = parseInt(
+      prompt("Que torta llevara? \n1)Chocolate\n2)Frutilla\n3)Crema")
+    );
+  } while (producto !== 1 && producto !== 2 && producto !== 3);
+
+  switch (producto) {
+    case 1:
+      return "Chocolate";
+    case 2:
+      return "Frutilla";
+    case 3:
+      return "Crema";
+  }
+}
+
+function validarPrecio(nombre) {
+  if (nombre === "Chocolate") {
+    return 350;
+  } else if (nombre === "Frutilla") {
+    return 200;
+  } else {
+    return 220;
+  }
+}
+
+
+function cobrar(producto, precio, nombre) {
+  alert(
+    nombre.toUpperCase() +
+      ", usted lleva la siguiente torta :" +
+      producto +
+      " y su precio es de :$" +
+      precio
+  );
+
+  let pago = parseInt(prompt("Con cuanto abona ? :"));
+  if (pago > precio) {
+    alert(" Gracias ! Su vuelto es de $" + (pago - precio));
+  } else {
+    alert("Saldo insuficiente, por favor deposite más dinero!");
+  }
+
+}
+
+let nombreCliente = saludo();
+let tortaTipo = listadoProductos();
+let precio = validarPrecio(tortaTipo);
+cobrar(tortaTipo, precio, nombreCliente);
+
+// Fin integrador
+
+
+
+
+
+/* saludar ("Bienvenido/a a Bakery Sunshine, Sr/Sra ", ingreso);
+
+function lista (param1, param2, param3, param4, param5) {
+    return param1 + " " + param2 + " " + param3 + " " + param4 + " " + param5
+}
+
+lista ("Chocolate", "frutilla", "Dulce de leche", "Merengue", "Crema chantilly");
+
+prompt("Elija su torta preferida: " + lista) */
